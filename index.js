cde2dc4b4fe46e829fa5851bce426fb4bda95124
yong liang <yongliang24@gmail.com>
2018-11-06 21:03:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-happyHolidaysTo(holiday, name){
+function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
 /*
