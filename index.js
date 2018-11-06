152eccaa7367b8bf59d7ed16b904a4a1cf18baa6
yong liang <yongliang24@gmail.com>
2018-11-06 20:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays(){
+  var s = "Happy holidays!";
+  return s;
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+
+happyHolidaysTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`;
+}
